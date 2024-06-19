e7d568515b9b883dbb10b02f34d983a18440d5a4
Rennan Rodrigues <rennan0rei@gmail.com>
2024-06-19 17:48:46 -0300

    Correção dos from e import

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-def sair():
-    print("Encerrando o programa...")
+from conversor_medidas import ConversorDeMedidas
+from conversor_temperatura import ConversorDeTemperatura
 
-def menu_principal():
-    while True:
-        print("\n*Calculadora de Conversão de Unidades*")
-        print("1. Conversor de Medidas")
-        print("2. Conversor de Temperaturas")
-        print("3. Sair")
-        opção = input("Digite a opção desejada: ")
-        if opção == "1":
-            from conversor_medidas import ConversorDeMedidas
-        elif opção == "2":
-            from conversor_temperatura import ConversorDeTemperatura
-        elif opção == "3":
-            sair()
-            break
-        else:
-            print("Opção inválida. Tente novamente.")
 
-menu_principal()
+print("\n*Calculadora de Conversão de Unidades*")
+print("1. Conversor de Centimetros para Metros")
+print("2. Conversor de Metros para Centimetros")
+print("3. Conversor de Fahrenheit para Celsius")
+print("4. Conversor Celsius para Fahrenheit")
+print("Digite qualquer coisa para Sair")
+
+opção = input("Digite a opção desejada: ")
+valor = float(input("Digite um valor: "))
+
+if opção == "1":
+        resultado = 
+elif opção == "2":
+    resultado = 
+elif opção == "3":
+    resultado =
+elif opção == "4":
+    resultado =
+else:
+    print("Saindo do programa")
